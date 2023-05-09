87f222a3bcd82aa5741e4a713feb6feaf84b5b93
logan-cic <logan.cc@outlook.com>
2023-05-09 16:05:50 -0300

    mudando exemplo de grafo

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,47 @@ public class Main {
     
     public static void main(String[] args) {
 
-        Grafo<String> grafo = new Grafo<String>();
-        grafo.addVertice("A");
-        grafo.addVertice("B");
-        grafo.addVertice("C");
-        grafo.addVertice("D");
-        grafo.addVertice("E");
-        grafo.addVertice("F");
+        Grafo<String> g = new Grafo<String>();
+
+        g.addVertice("0");
+        g.addVertice("1");
+        g.addVertice("2");
+        g.addVertice("3");
+        g.addVertice("4");
+        g.addVertice("5");
+        g.addVertice("6");
+        g.addVertice("7");
+        g.addVertice("8");
+
+
+        g.addAresta("0","1");
+        g.addAresta("1","0");
+        g.addAresta("1","2");
+        g.addAresta("2","1");
+        g.addAresta("4","1");
+        g.addAresta("1","4");
+        g.addAresta("2","3");
+        g.addAresta("3","2");
+        g.addAresta("3","4");
+        g.addAresta("4","3");
+        g.addAresta("4","5");
+        g.addAresta("5","4");
+        g.addAresta("4","6");
+        g.addAresta("6","4");
+        g.addAresta("6","7");
+        g.addAresta("7","6");
+        g.addAresta("6","8");
+        g.addAresta("8","6");
+
 
         
-        grafo.addAresta("A", "B");
-        grafo.addAresta("A", "C");
-
-        grafo.addAresta("B", "C");
-        grafo.addAresta("B", "A");
-
-
-        grafo.addAresta("C", "B");
-        grafo.addAresta("C", "D");
-
-
-        grafo.addAresta("D", "E");
-        grafo.addAresta("D", "C");
-
-        grafo.addAresta("E", "D");
-        
-        grafo.addAresta("E", "F");
-        grafo.addAresta("F", "F");
-        grafo.addAresta("F", "E");
-
-        
-        grafo.DFS();
-        System.out.println(grafo.toString());
+        g.DFS();
+        System.out.println(g.toString());
 
         System.out.println(" ");
-        grafo.printAdjList();
+        g.printAdjList();
         System.out.println(" ");
-        grafo.printMatrizAdj();
+        g.printMatrizAdj();
 
         System.out.println(" ");
  
